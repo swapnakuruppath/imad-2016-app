39cd9d5ca17392deeef4caf956c647d99b6e2737
swapnakuruppath <swapnak1983@gmail.com>
2016-09-26 17:05:45 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one',function(req,res)
 {
-res.send(createtemplate(articleone));
+res.send(createTemplate(articleone));
 
 });
 app.get('/article-two',function(req,res)
