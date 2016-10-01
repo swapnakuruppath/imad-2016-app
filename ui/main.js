f614a45e619d2a005a8f1910641eb0c3e92a9a17
swapnakuruppath <swapnak1983@gmail.com>
2016-10-01 14:01:37 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getelementbyid('main-text');
-element.innerhtml='new value';
+//var element=document.getelementbyid('main-text');
+//element.innerhtml='new value';
