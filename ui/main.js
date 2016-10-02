b7463825b1aa4e33e6898b03308bb4b2c08db55c
swapnakuruppath <swapnak1983@gmail.com>
2016-10-02 16:45:36 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHtml='New value';
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+};
+
 img.onclick=function()
 {
-    img.style.marginleft='100px';
+    var interval=setInterval(moveRight,100);
 };
