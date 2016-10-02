6b34f3f8af1757ded7c5d58c05759ec9e3e4de30
swapnakuruppath <swapnak1983@gmail.com>
2016-10-02 11:33:56 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementbyid('main-text');
 element.innerHtml='new value';
-var img=document.getElementbyid('img');
+var img=document.getElementbyid('madi');
 img.onclick=function()
 {
     img.style.marginleft='100px';
