5ecb46bd5b85857b3edfa67e8c1402a837bf767a
swapnakuruppath <swapnak1983@gmail.com>
2016-10-03 21:24:16 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
 var names=[];
 app.get('/submit-name',function(req,res)
 {
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
    res.send(JSON.stringify(names));
 
