bab77c8404e4177cedb710cfbcce3761c42b2841
swapnakuruppath <swapnak1983@gmail.com>
2016-10-03 17:55:47 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick=function()
             {
                 var counter=request.responseText;
                  var span=document.getElementById('count');
-    span.InnerHTML=counter.toString();
+    span.innerHTML=counter.toString();
             }
         }
     };
