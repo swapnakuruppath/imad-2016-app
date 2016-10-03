4df8368cde279a13a280d268137533817c336822
swapnakuruppath <swapnak1983@gmail.com>
2016-10-03 19:39:28 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick=function()
             var list='';
             for(var i=0;i<names.length;i++)
             {
-                list+='<li>'+names[i]+'/li';
+                list+='<li>'+names[i]+'</li>';
                             }
                             var ul=document.getElementById('namelist');
                             ul.innerHTML=list;
