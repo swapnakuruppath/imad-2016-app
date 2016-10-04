dad87c07a68ed362b989221af5f0b89c6d0c07ed
swapnakuruppath <swapnak1983@gmail.com>
2016-10-04 21:32:56 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 
 var button=document.getElementById('counter');
+button.onclick=function()
+{
  request.onreadystatechange=function()
               {
             if(request.readyState===XMLHttpRequest.DONE)
