cd06fc62a4e0156c7ed2c8e4d82307374404cd8b
swapnakuruppath <swapnak1983@gmail.com>
2016-10-04 19:02:50 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,13 @@
 
 var button=document.getElementById('counter');
-
 button.onclick=function()
 {
                 var counter=request.responseText;
                  var span=document.getElementById('count');
     span.innerHTML=counter.toString();
-            
 };     
-    
     request.open('GET','http://swapnakuruppath.imad.hasura-app.io/counter',true);
     request.send(null);
-        
         var nameInput=document.getElementById('name');
         var name =nameInput.value;
         var submit=document.getElementById('submit_btn');
