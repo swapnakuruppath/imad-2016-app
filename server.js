1909e7d2b17a0ed07bfb1d71a6ca17385d9dd443
swapnakuruppath <swapnak1983@gmail.com>
2016-10-24 18:58:14 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ var pool = new Pool(config);
 
 app.get('/test-db',function(req,res)
 {
-    pool.query('SELECT * FROM test',fun(err,result)
+    pool.query('SELECT * FROM test',function(err,result)
     {
        if (err)
        {
