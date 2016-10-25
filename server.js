cd8761e7a42edb4d0f8ef4f2fd5c160419b6cb2b
swapnakuruppath <swapnak1983@gmail.com>
2016-10-25 13:16:39 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,12 +112,26 @@ app.get('/submit-name',function(req,res)
 
 
 
-app.get('/:articlename',function(req,res)
+app.get('/articles/:articleName',function(req,res)
 {
-    var articlename=req.params.articlename;
-res.send(createTemplate(articles[articlename]));
-
-});
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result)
+    {
+        if(err)
+        {
+            res.status(500),send(err.tostring());
+                    }
+        else if(result.rows.length===0)
+        {
+            res.status(400),send('Article not found');
+        }
+        else
+        {
+            var articleData=result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+        
+    });
+   });
 app.get('/article-two',function(req,res)
 {
    res.send(createTemplate(articletwo));
